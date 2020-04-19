d5e8ef8fe99e3d66e79c785758726f904edcd018
teieigun <teieigun@gmail.com>
2020-04-19 09:05:46 +0900

    Default Changelist

---
+++ src/main/java/com/microservice/edu/constants/MicroServiceConstants.java
@@ -0,0 +1,13 @@
+package com.microservice.edu.constants;
+
+public final class MicroServiceConstants {
+
+	//省番号
+	public static final String CODE_SHENG ="01";
+
+	//用户状态 0：为认证状态的用户
+	public static final int USER_STATUS_NOPASS =0;
+
+	//用户状态 1：通过认证状态的用户
+	public static final int USER_STATUS_PASSED =1;
+}
