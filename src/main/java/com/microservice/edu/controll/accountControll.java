a70d5caed6b1c0c6fd98a13683926cde0db04ce7
teieigun <teieigun@gmail.com>
2020-05-10 14:33:10 +0900

    brach004 video/css 删除

--- src/main/java/com/microservice/edu/controll/accountControll.java
+++ src/main/java/com/microservice/edu/controll/accountControll.java
@@ -1,20 +1,17 @@
 package com.microservice.edu.controll;
 
-import Decoder.BASE64Decoder;
+
 import com.microservice.edu.form.UploadForm;
-import com.microservice.edu.service.TopPageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import sun.misc.BASE64Decoder;
 import web.SessionContext;
 
 import javax.servlet.http.HttpServletRequest;
